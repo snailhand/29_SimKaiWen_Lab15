f555756cebb498cd4ef68832dc11eacfec7a5a52
simkaiwen <kaiphonegt@gmail.com>
2021-01-14 21:47:47 +0800

    Lab 15

---
+++ 29_SimKaiWen_Lab15/Assets/Scripts/BGM.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGM : MonoBehaviour
+{
+    public AudioClip[] bgmClip;
+    public AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int rand = Random.Range(0, bgmClip.Length);
+        audioSource.PlayOneShot(bgmClip[rand]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            audioSource.volume += 0.4f * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            audioSource.volume -= 0.4f * Time.deltaTime;
+        }
+    }
+}
---
+++ 29_SimKaiWen_Lab15/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterController : MonoBehaviour
+{
+    public GameObject jumpText;
+    public AudioSource audioSource;
+    public AudioClip[] audioArray;
+
+    private int jumpCount = 0;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpCount++;
+
+            jumpText.GetComponent<Text>().text = "Jump: " + jumpCount;
+            rb.AddForce(new Vector3(0, 250, 0));
+
+            //audioSource.Play();
+            int rand = Random.Range(0, audioArray.Length);
+            audioSource.PlayOneShot(audioArray[rand]);
+
+        }
+    }
+}
